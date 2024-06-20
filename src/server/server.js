bc2c072b960cd7f11a2c06c5e40ac145afe4bf23
andiichwannn <andiichwann@gmail.com>
2024-06-20 16:49:22 +0800

    change port

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { loadTemplate, loadCombinedData, loadFinalData } = require('../services/loadTemplate');
 const { handleChatRequest } = require('./handler');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.json());
 
